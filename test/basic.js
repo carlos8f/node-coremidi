cd457bb7400dbad3440a148c0db32f4a5fb67092
Carlos Rodriguez <carlos@s8f.org>
2012-09-02 19:38:58 -0700

    add test

--- test/basic.js
+++ test/basic.js
@@ -1,15 +1,33 @@
-var api = require('midi-api')
+var api = require('midi-api')({end: true})
   , coremidi = require('../')
 
 describe('basic test', function () {
-  it('plays a note', function (done) {
-    api({end: true})
-      .bank(0)
-      .program(0)
-      .noteOn(60)
-      .rest(2000)
-      .noteOff()
-      .pipe(coremidi.stream())
-      .once('end', done);
+  it('plays chords', function (done) {
+    api
+      .bank(2)
+      .program(33)
+      .rest(500)
+
+    function maj7 (root) {
+      api
+        .noteOn(root)
+        .noteOn(root + 4)
+        .noteOn(root + 7)
+        .noteOn(root + 11)
+        .rest(200)
+    }
+
+    maj7(44)
+    api.noteOff();
+    maj7(45)
+    api.noteOff();
+    maj7(46)
+    api.noteOff();
+    maj7(47)
+    api.rest(1000);
+    api.noteOff();
+    api.rest(500);
+
+    api.pipe(coremidi.stream()).once('end', done);
   });
 });
